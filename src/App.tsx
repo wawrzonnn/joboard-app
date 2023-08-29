6b69d290c071c8d46f9a1a897245845d03542993
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-08-29 14:11:40 +0200

    feat: added filtering by location

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,26 @@ import { Container } from './components/Container/Container';
 import { FiltersContainer } from './components/FiltersContainer/FiltersContainer';
 import { OffersContainer } from './components/OffersContainer/OffersContainer';
 import { QueryClientProvider } from 'react-query';
-import { queryClient } from './api/queryClient'
+import { queryClient } from './api/queryClient';
 
 function App() {
    const [selectedJobTypes, setSelectedJobTypes] = useState<string[]>([]);
-   const [ selectedSeniority, setSelectedSeniority] = useState<string[]>([]);
+   const [selectedSeniority, setSelectedSeniority] = useState<string[]>([]);
+   const [selectedLocation, setSelectedLocation] = useState<string[]>([]);
    return (
       <QueryClientProvider client={queryClient}>
          <Container>
             <div className="container">
-               <FiltersContainer setSelectedJobTypes={setSelectedJobTypes} setSelectedSeniority={setSelectedSeniority} />
-               <OffersContainer selectedJobTypes={selectedJobTypes} selectedSeniority={selectedSeniority} />
+               <FiltersContainer
+                  setSelectedJobTypes={setSelectedJobTypes}
+                  setSelectedSeniority={setSelectedSeniority}
+                  setSelectedLocation={setSelectedLocation}
+               />
+               <OffersContainer
+                  selectedJobTypes={selectedJobTypes}
+                  selectedSeniority={selectedSeniority}
+                  selectedLocation={selectedLocation}
+               />
             </div>
          </Container>
       </QueryClientProvider>
