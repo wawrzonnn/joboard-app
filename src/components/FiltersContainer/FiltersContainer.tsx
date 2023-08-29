7ccbb4f0517b5cd390ecb62f39047d2c6fccd491
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-08-29 21:39:00 +0200

    feat: added filters mobile ui

--- src/components/FiltersContainer/FiltersContainer.tsx
+++ src/components/FiltersContainer/FiltersContainer.tsx
@@ -7,28 +7,52 @@ import { ClearButton } from '../ClearButton/ClearButton';
 import FiltersButton from '../FiltersButton/FiltersButton';
 import { JobType, Seniority, WorkLocation } from '../../api/types';
 import { useFilters } from '../../contexts/FilterContext';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
 const jobType = Object.values(JobType);
 const seniority = Object.values(Seniority);
 const location = Object.values(WorkLocation);
 
 export const FiltersContainer = () => {
-   const { setSelectedJobTypes, setSelectedSeniority, setSelectedLocation, setSelectedSalary, clearAllFilters } =
-      useFilters();
+   const {
+      setSelectedJobTypes,
+      setSelectedSeniority,
+      setSelectedLocation,
+      setSelectedSalary,
+      clearAllFilters,
+   } = useFilters();
 
    const [clearFiltersCount, setClearFiltersCount] = useState<number>(0);
+   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
    const handleClearFilters = () => {
-      clearAllFilters()
+      clearAllFilters();
       setClearFiltersCount((prev) => prev + 1);
    };
 
+   const handleOpenMenu = () => {
+      setOpenMenu(!openMenu);
+   };
+
+   const getMobileMenuClasses = cx({
+      [styles.filters_wrapper]: true,
+      [styles.hidden]: !openMenu
+   });
+
+   const getMobileHeaderClasses = cx({
+      [styles.header_mobile]: true,
+      [styles.header_mobile_padding]: openMenu
+   });
+
+
    return (
       <div className={styles.container}>
-         <div className={styles.header_mobile}>
+         <div className={getMobileHeaderClasses}>
             <Logo />
-            <FiltersButton />
+            <FiltersButton onClick={handleOpenMenu} />
          </div>
-         <div className={styles.filters_wrapper}>
+         <div className={getMobileMenuClasses}>
             <header className={styles.header}>
                <p className={styles.header_title}>Filter offers</p>
                <ClearButton onClick={handleClearFilters}>Clear filters</ClearButton>
