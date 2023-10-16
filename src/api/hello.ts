6a6df5d3c6f7877fdb118eafca23db8b26d10a67
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-10-16 16:55:20 +0200

    refactor: removed temporary endpoints

--- src/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { NextRouteHandler } from 'next/types';
-
-const helloHandler: NextRouteHandler = async (req, res) => {
-  console.log('hello');
-  res.status(200).send('Check your server console.');
-};
-
-export default helloHandler;
